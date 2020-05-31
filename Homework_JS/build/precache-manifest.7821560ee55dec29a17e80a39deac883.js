d2570d948b0ce785def86e86138585c9f3132556
lerakrya8 <55090716+lerakrya8@users.noreply.github.com>
2020-05-31 23:04:43 +0300

    Add files via upload

---
+++ Homework_JS/build/precache-manifest.7821560ee55dec29a17e80a39deac883.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3016dac87ccf82e5b8bf66de8c33c7c",
+    "url": "/Homework_JS/index.html"
+  },
+  {
+    "revision": "f40b11a1d3d30626ee00",
+    "url": "/Homework_JS/static/css/main.72341cae.chunk.css"
+  },
+  {
+    "revision": "11ad9b8e290e2f9b6e67",
+    "url": "/Homework_JS/static/js/2.1d010302.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/Homework_JS/static/js/2.1d010302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40b11a1d3d30626ee00",
+    "url": "/Homework_JS/static/js/main.247ad9c0.chunk.js"
+  },
+  {
+    "revision": "ff513c9d6cefd378c04d",
+    "url": "/Homework_JS/static/js/runtime-main.5e9a15ca.js"
+  },
+  {
+    "revision": "cccdc452b447c69944a0b5239b71b2d3",
+    "url": "/Homework_JS/static/media/Beautiful-nature-landscape-mountains-top-view-clouds-fog-morning_2560x1600.cccdc452.jpg"
+  },
+  {
+    "revision": "ba80d2b3112f3142f67ee256487c8988",
+    "url": "/Homework_JS/static/media/unnamed.ba80d2b3.png"
+  }
+]);
